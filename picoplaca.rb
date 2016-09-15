4a5bc946bde67613e84d0aca915b7ae7023c7af0
mariohernandezv <mariohernandezv@yahoo.com.ar>
2016-09-14 22:26:33 -0500

    Car class

--- picoplaca.rb
+++ picoplaca.rb
@@ -1,24 +1,32 @@
-def picoplaca(plate = "PYX0989", date = "2016-09-18", time = "18:43:22" )
-	
-	require 'time'
-	t = Time.parse(date + " " + time)
+class Car
 
-	if ( 65959 < t.strftime("%H%M%S").to_i && t.strftime("%H%M%S").to_i < 93000) || (155959 < t.strftime("%H%M%S").to_i && t.strftime("%H%M%S").to_i < 193000)
-		case plate[-1,1].to_i
-		when 1,2
-			t.strftime("%A") == "Monday"
-		when 3,4
-			t.strftime("%A") == "Tuesday"
-		when 5,6
-			t.strftime("%A") == "Wednesday"
-		when 7,8
-			t.strftime("%A") == "Thursday"
-		when 9,0
-			t.strftime("%A") == "Friday" 
+	def initialize(plate)
+		@plate = plate
+	end
+
+	def picoplaca(plate = @plate, date = "2016-09-18", time = "18:43:22" )
+		
+		require 'time'
+		t = Time.parse(date + " " + time)
+
+		if ( 65959 < t.strftime("%H%M%S").to_i && t.strftime("%H%M%S").to_i < 93000) || (155959 < t.strftime("%H%M%S").to_i && t.strftime("%H%M%S").to_i < 193000)
+			case plate[-1,1].to_i
+			when 1,2
+				t.strftime("%A") == "Monday"
+			when 3,4
+				t.strftime("%A") == "Tuesday"
+			when 5,6
+				t.strftime("%A") == "Wednesday"
+			when 7,8
+				t.strftime("%A") == "Thursday"
+			when 9,0
+				t.strftime("%A") == "Friday" 
+			else
+				false
+			end
 		else
 			false
 		end
-	else
-		false
 	end
+
 end
